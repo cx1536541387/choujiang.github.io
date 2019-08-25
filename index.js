84144380122b02fbfc125c7414fda892fb45c6e6
cx1536541387 <54475674+cx1536541387@users.noreply.github.com>
2019-08-25 12:45:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ var $span = $(".rotate-bg span");
                 top: '50%',
                 opacity: 1
             });
-            $(".mask-bg").show().css('background-color', 'rgba(34,47,62 0.3)');
+            $(".mask-bg").show().css('background-color', 'rgba(34,47,62,0.3)');
         }
 
         $(".prompt .btn,.prompt i").on('click', function() {
